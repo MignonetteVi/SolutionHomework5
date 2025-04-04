38566bb1bd45e0d6343cd8566da5690a0b09e5a3
Kristina <tolstovachristina@gmail.com>
2025-04-04 23:36:08 +0500

    Update HighResolutionImage.java

--- src/HighResolutionImage.java
+++ src/HighResolutionImage.java
@@ -1,2 +1,17 @@
-public class HighResolutionImage {
+public class HighResolutionImage implements Image{
+    private String filepath;
+
+    public HighResolutionImage(String filepath) {
+        this.filepath = filepath;
+    }
+
+    @Override
+    public void displayThumbnail() {
+        System.out.println("Миниатюра: " + filepath);
+    }
+
+    @Override
+    public void displayFullImage() {
+        System.out.println("Изображение: " + filepath);
+    }
 }
