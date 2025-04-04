c12161281e29c0839a29641299140cb6513d8f6a
Kristina <tolstovachristina@gmail.com>
2025-04-05 00:11:48 +0500

    Update MarkerStyle.java

--- src/MarkerStyle.java
+++ src/MarkerStyle.java
@@ -1,2 +1,15 @@
 public class MarkerStyle {
+    private String icon;
+    private String color;
+    private String labelStyle;
+
+    public MarkerStyle(String icon, String color, String labelStyle) {
+        this.icon = icon;
+        this.color = color;
+        this.labelStyle = labelStyle;
+    }
+
+    public void draw(int x, int y) {
+        System.out.println("Маркер '" + icon + "' в (" + x + ", " + y + ") цвет: " + color);
+    }
 }
