ab81aae591abdf89702a9fc8cac4d1800f519f46
Kristina <tolstovachristina@gmail.com>
2025-04-05 00:28:20 +0500

    Update LocationMarker.java

--- src/LocationMarker.java
+++ src/LocationMarker.java
@@ -1,2 +1,14 @@
 public class LocationMarker {
+    private int x, y;
+    private MarkerStyle style;
+
+    public LocationMarker(int x, int y, MarkerStyle style) {
+        this.x = x;
+        this.y = y;
+        this.style = style;
+    }
+
+    public void render() {
+        style.draw(x, y);
+    }
 }
