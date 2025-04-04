da0149a8af79d5c8df6016b9f1e972c04305197f
Kristina <tolstovachristina@gmail.com>
2025-04-04 23:40:13 +0500

    Create DiskLoader.java

---
+++ src/DiskLoader.java
@@ -0,0 +1,6 @@
+// Класс для только примера загрузки с диска (чтобы было логичнее, его правда реализация отсутствует)
+public class DiskLoader {
+    public static void load(String filepath) {
+        System.out.println("Загрузка с диска... " + filepath);
+    }
+}
