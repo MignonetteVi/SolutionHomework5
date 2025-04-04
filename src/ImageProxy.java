0f91ab7c783dc22d7e388c34aa042c6a787c3784
Kristina <tolstovachristina@gmail.com>
2025-04-04 23:53:11 +0500

    Update ImageProxy.java

--- src/ImageProxy.java
+++ src/ImageProxy.java
@@ -1,5 +1,12 @@
 public class ImageProxy implements Image{
     private String filepath;
+    private HighResolutionImage realImage;
+
+    public ImageProxy(String filepath, HighResolutionImage realImage) {
+        this.filepath = filepath;
+        this.realImage = realImage;
+    }
+
     @Override
     public void displayThumbnail() {
         System.out.println("Прокси! Показ миниатюры: " + filepath);
@@ -7,6 +14,9 @@ public class ImageProxy implements Image{
 
     @Override
     public void displayFullImage() {
-
+        if (realImage == null) {
+            realImage = new HighResolutionImage(filepath);
+        }
+        realImage.displayFullImage();
     }
 }
