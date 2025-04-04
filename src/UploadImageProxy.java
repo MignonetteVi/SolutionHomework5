3afcc3e8251583631124b877653754bd1ae85c03
Kristina <tolstovachristina@gmail.com>
2025-04-05 00:05:58 +0500

    Update UploadImageProxy.java

--- src/UploadImageProxy.java
+++ src/UploadImageProxy.java
@@ -1,2 +1,15 @@
 public class UploadImageProxy {
+    private String agentName;
+
+    public UploadImageProxy(String agentName) {
+        this.agentName = agentName;
+    }
+
+    public void upload(String imageName) {
+        if (AgentAuth.isLoggedIn(agentName)) {
+            System.out.println("Загрузка! Агент '" + agentName + "' загрузил: " + imageName);
+        } else {
+            System.out.println("Ошибка! Неавторизованный пользователь не может загружать изображения.");
+        }
+    }
 }
